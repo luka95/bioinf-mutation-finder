3365cedbf79f0c377a0f7456c9675b3f6c969168
dorian <dorian14126@live.com>
2019-01-13 15:00:21 +0100

    updated main method

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,39 @@ char findMostFrequentValue(vector<char> &values);
 
 int main() {
 
+//    string s = "AGTGTA";
+//    string t = "TAGCGTT";
+//
+//    zw alignment = Hirschberg(s,t);
+//    string z = alignment.z;
+//    string w = alignment.w;
+//    int genome_pos = 0;
+//    vector<Mutation> mutations;
+//
+//    for (int i = 0, n = z.length(); i < n; i++) {
+//        char c = z[i];
+//        if (c == '-') {
+//            //insertion - mark = lower case base
+//            mutations.push_back(Mutation(MutationType::Insertion, genome_pos, w[i]));
+//        } else {
+//            // '-' in read_align is for deletion, a letter is for supstitution or match
+//            if(w[i] == '-'){
+//                //deletion
+//                mutations.push_back(Mutation(MutationType::Deletion, genome_pos,'-'));
+//            }else{
+//                if(w[i]!=c){
+//                    //supstitution
+//                    mutations.push_back(Mutation(MutationType::Substitution, genome_pos,w[i]));
+//                }
+//            }
+//            genome_pos++;
+//        }
+//    }
+//
+//    for(auto &mutation : mutations){
+//        cout << mutation;
+//    }
+
     DataLoader data_loader(genome_path, mutated_path);
     data_loader.loadData();
 
@@ -36,11 +69,12 @@ int main() {
 
     int processed = 1;
     int total = data_loader.mutated_genome_reads.size();
+    int mapped = 0;
 
     //TODO Open MP for parallelization here
     for (string &read : data_loader.mutated_genome_reads) {
 
-        cout<<"Processing read "<<processed<<" of "<<total<<endl;
+        cout << "Processing read " << processed << " of " << total << endl;
         processed++;
 
         read_index = Index::index(read, w, k);
@@ -49,6 +83,7 @@ int main() {
         int strand_xor = get<1>(mapping);
         if (strand_xor == -1) continue;
 
+        mapped++;
         tuple<int, int, int, int> positions = get<0>(mapping);
         int genome_start = get<0>(positions);
         int genome_end = get<1>(positions);
@@ -66,33 +101,58 @@ int main() {
         string reg_align = alignment.z;
         string read_align = alignment.w;
 
+        int genome_pos = genome_start;
         for (int i = 0, n = reg_align.length(); i < n; i++) {
             char c = reg_align[i];
             if (c == '-') {
                 //insertion - mark = lower case base
-                alignments[genome_start + i].push_back(static_cast<char &&>(tolower(read_align[i])));
+                //mutations.push_back(Mutation(MutationType::Insertion, genome_pos, read_align[i]));
+                alignments[genome_pos].push_back(static_cast<char &&>(tolower(read_align[i])));
             } else {
                 // '-' in read_align is for deletion, a letter is for supstitution or match
-                alignments[genome_start + i].push_back(read_align[i]);
+//                if(read_align[i] == '-'){
+//                    //deletion
+//                    mutations.push_back(Mutation(MutationType::Deletion, genome_pos,'-'));
+//                }else{
+//                    if(read_align[i]!=c) {
+//                        //supstitution
+//                        mutations.push_back(Mutation(MutationType::Substitution, genome_pos, read_align[i]));
+//                    }
+//                }
+                alignments[genome_pos].push_back(read_align[i]);
+                genome_pos++;
+
             }
         }
     }
 
+    cout<< "Mapped "<<mapped<<" of "<<total<<endl;
 
     //collect mutations
     for (int i = 0, n = data_loader.genome.length(); i < n; i++) {
         vector<char> position_alignments = alignments[i];
-        if(position_alignments.size() == 0) continue;
+        if (position_alignments.size() <=10) {
+            continue;
+        }
+
         char c = findMostFrequentValue(position_alignments);
+        if (c == '\0') {
+            continue;
+        }
+
+        char actual = data_loader.genome[i];
         if (islower(c)) {
             //insertion
             mutations.push_back(Mutation(MutationType::Insertion, i, c));
+            continue;
         } else if (c == '-') {
             //deletion
             mutations.push_back(Mutation(MutationType::Deletion, i, c));
+            continue;
         } else if (c != data_loader.genome[i]) {
             //supstitution
             mutations.push_back(Mutation(MutationType::Substitution, i, c));
+            continue;
         }
     }
 
@@ -100,17 +160,18 @@ int main() {
     //output mutations
     ofstream file;
     file.open(results_path);
-    for(auto &mutation : mutations){
-        file<<mutation;
+    for (auto &mutation : mutations) {
+        file << mutation;
     }
 
     return 0;
 }
 
 char findMostFrequentValue(vector<char> &values) {
+    if (values.empty()) return '\0';
 
-    int max = -1;
-    char mf_value;
+    int max = 1;
+    char mf_value = values[0];
     int cnt = 1;
 
     sort(values.begin(), values.end());
